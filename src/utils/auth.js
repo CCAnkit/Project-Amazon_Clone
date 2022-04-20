dbfd6110116bc0c82bf94ddcb5ac8530ab974fa5
CCAnkit <ankitgupta9828@gmail.com>
2022-04-20 15:05:41 +0530

    Project Completed

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -8,15 +8,25 @@ const userAuth = async (req, res, next) => {
     if (!token) {     //validating the token is present in headers or not.
       return res.status(403).send({ status: false, message: 'Token must be present' });
     }
-    
+
     let validToken = token.split(' ')    //
 
     const decodeToken = jwt.verify(validToken[1], "Project-ShoppingCart", {ignoreExpiration: true})    //If present then verify the secret key
     if (!decodeToken) {
       return res.status(403).send({ status: false, message: 'You are not autherised to access.' });
     }
+
+    let expiration = decodeToken.exp
+      
+    let tokenExtend = Math.floor(Date.now() / 1000) 
+
+    if (expiration < tokenExtend){
+      return res.status(401).send({ status: false, message: "Token expired" })
+    }
+
     req.userId = decodeToken.userId     
-        next();     //if token is present & for the same user then move to the next
+        
+      next();     //if token is present & for the same user then move to the next
     } 
     catch (err) {
         console.log(err);
